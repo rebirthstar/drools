32770ad78ec653e2ae52bdba7c156a1bcb39cdbf
luoteng <carsonluo@shenlanbao.com>
2020-09-14 15:01:33 +0800

    add

---
+++ src/main/java/com/slb/droolsdemo/DroolsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.slb.droolsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slb/droolsdemo/DroolsdemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.slb.droolsdemo;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsdemoApplicationTests {
+
+
+
+}
